485d5037f871f8047994b6194f96046541b0f2ef
yara121 <yara767@gmail.com>
2023-09-03 15:18:26 +0300

    Updates

---
+++ precache-manifest.e2e070f75265882a2eaa80446aaf09f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b2b54e3f187db9a6142f216d0d7103",
+    "url": "/Netflix/index.html"
+  },
+  {
+    "revision": "23e2e53c45f523b5cb30",
+    "url": "/Netflix/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "23e2e53c45f523b5cb30",
+    "url": "/Netflix/static/js/2.48fcc7db.chunk.js"
+  },
+  {
+    "revision": "ac9c09499ba9e4be740729c250300ba4",
+    "url": "/Netflix/static/js/2.48fcc7db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e9680c71e5527eab45",
+    "url": "/Netflix/static/js/main.3ef5c709.chunk.js"
+  },
+  {
+    "revision": "f0085d1d9f9b9b1c0bbb",
+    "url": "/Netflix/static/js/runtime-main.eb990e6e.js"
+  },
+  {
+    "revision": "6dbba458959d4ce1edd2f5b3ab3ae13b",
+    "url": "/Netflix/static/media/logo.6dbba458.svg"
+  }
+]);
